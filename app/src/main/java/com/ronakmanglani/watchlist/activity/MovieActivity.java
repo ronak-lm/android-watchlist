55995132d126d728cc048b76afc958a9125d896f
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-05 01:09:15 +0530

    Remove: Unused imports

--- app/src/main/java/com/ronakmanglani/watchlist/activity/MovieActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/MovieActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
