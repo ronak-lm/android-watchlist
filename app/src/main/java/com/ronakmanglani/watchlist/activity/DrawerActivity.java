205fb01150b18cfc48f302fb70dcc7e657cd0829
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-05 23:44:55 +0530

    Add code to bind views to class objects in DrawerActivity

--- app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
@@ -1,16 +1,27 @@
 package com.ronakmanglani.watchlist.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.ronakmanglani.watchlist.R;
 
+import butterknife.Bind;
+
 public class DrawerActivity extends AppCompatActivity {
+
+    private String[] drawerList;
+    @Bind(R.id.drawer_layout)
+    private DrawerLayout drawerLayout;
+    @Bind(R.id.drawer_recycler)
+    private RecyclerView recyclerView;
 
     // Start activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+        drawerList = getResources().getStringArray(R.array.drawer_list);
     }
 }
