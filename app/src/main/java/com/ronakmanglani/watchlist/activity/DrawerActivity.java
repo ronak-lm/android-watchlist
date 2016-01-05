8b356592bb774d4fa0deba8245759f585581eef1
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-05 23:16:40 +0530

    Create java class for DrawerActivity and add to manifest

---
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
@@ -0,0 +1,16 @@
+package com.ronakmanglani.watchlist.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ronakmanglani.watchlist.R;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    // Start activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
