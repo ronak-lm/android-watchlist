a3d64feb378174a62125ef3bbcd641da79e0a256
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-06 23:59:23 +0530

    Add listener for NavigationView

--- app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
@@ -1,10 +1,12 @@
 package com.ronakmanglani.watchlist.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,7 @@ public class DrawerActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar) Toolbar toolbar;
     @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @Bind(R.id.navigation_view) NavigationView navigationView;
 
     // Start activity
     @Override
@@ -26,24 +29,51 @@ public class DrawerActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.app_name, R.string.app_name) {
+        // Respond to clicks of NavigationView
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (!item.isChecked()) {
+                    item.setChecked(true);
+                }
 
+                drawerLayout.closeDrawers();
+
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.drawer_popular:
+                        return true;
+                    case R.id.drawer_rated:
+                        return true;
+                    case R.id.drawer_upcoming:
+                        return true;
+                    case R.id.drawer_playing:
+                        return true;
+                    case R.id.drawer_favorite:
+                        return true;
+                    case R.id.drawer_watchlist:
+                        return true;
+                    default: return false;
+                }
+            }
+        });
+
+        // Respond to opening/closing of navigation drawer
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name) {
             @Override
             public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
                 super.onDrawerClosed(drawerView);
             }
-
             @Override
             public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
                 super.onDrawerOpened(drawerView);
             }
         };
 
-        //Setting the actionbarToggle to drawer layout
+        //Setting the ActionBarToggle to DrawerLayout
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+        // Add hamburger icon to Toolbar
         actionBarDrawerToggle.syncState();
     }
 }
