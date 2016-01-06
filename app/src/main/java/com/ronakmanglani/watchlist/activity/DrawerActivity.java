03a1dce9a5907face177fc013fb8261a42d86a5f
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-06 23:47:40 +0530

    Create drawer activity and setup drawer items

--- app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/DrawerActivity.java
@@ -2,26 +2,48 @@ package com.ronakmanglani.watchlist.activity;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.ronakmanglani.watchlist.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DrawerActivity extends AppCompatActivity {
 
-    private String[] drawerList;
-    @Bind(R.id.drawer_layout)
-    private DrawerLayout drawerLayout;
-    @Bind(R.id.drawer_recycler)
-    private RecyclerView recyclerView;
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
 
     // Start activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
-        drawerList = getResources().getStringArray(R.array.drawer_list);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.app_name, R.string.app_name) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
     }
 }
