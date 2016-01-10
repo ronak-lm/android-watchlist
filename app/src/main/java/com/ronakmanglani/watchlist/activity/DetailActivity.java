352e4b90e1bab4e97083dd74c6534fd84315683c
Ronak Manglani <ronak.manglani@gmail.com>
2016-01-10 23:23:12 +0530

    Move layout views from detail activity to detail fragment

--- app/src/main/java/com/ronakmanglani/watchlist/activity/DetailActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/DetailActivity.java
@@ -15,8 +15,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
--- app/src/main/java/com/ronakmanglani/watchlist/fragment/DetailFragment.java
+++ app/src/main/java/com/ronakmanglani/watchlist/fragment/DetailFragment.java
@@ -8,17 +8,10 @@ import android.view.ViewGroup;
 
 import com.ronakmanglani.watchlist.R;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class DetailFragment extends Fragment {
 
-    public DetailFragment() {
-    }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 }
