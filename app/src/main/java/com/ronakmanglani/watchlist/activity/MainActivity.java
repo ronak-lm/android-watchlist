08f5fb2b08c4d8305e16c932033a6f17fd56ae82
Ronak Manglani <ronak.manglani@gmail.com>
2016-02-04 00:06:21 +0530

    Optimize imports

--- app/src/main/java/com/ronakmanglani/watchlist/activity/MainActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/MainActivity.java
@@ -2,12 +2,10 @@ package com.ronakmanglani.watchlist.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.ronakmanglani.watchlist.R;
 import com.ronakmanglani.watchlist.fragment.DetailFragment;
 
-import butterknife.Bind;
 import butterknife.BindBool;
 import butterknife.ButterKnife;
 
--- app/src/main/java/com/ronakmanglani/watchlist/activity/ReviewDetailActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/ReviewDetailActivity.java
@@ -2,19 +2,14 @@ package com.ronakmanglani.watchlist.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.ronakmanglani.watchlist.R;
 import com.ronakmanglani.watchlist.model.Review;
-import com.ronakmanglani.watchlist.util.TMDBHelper;
-import com.ronakmanglani.watchlist.util.YoutubeHelper;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
