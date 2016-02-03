25e51c3e071255e7662045906848ff82724ce071
Ronak Manglani <ronak.manglani@gmail.com>
2016-02-03 23:43:00 +0530

    Add function to load fragment

--- app/src/main/java/com/ronakmanglani/watchlist/activity/MainActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ronakmanglani.watchlist.R;
+import com.ronakmanglani.watchlist.fragment.DetailFragment;
+
+import butterknife.BindBool;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    public void loadDetailFragmentWith(String movieId) {
+        DetailFragment fragment = new DetailFragment();
+        Bundle args = new Bundle();
+        args.putString(DetailActivity.MOVIE_ID, movieId);
+        fragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction().replace(R.id.detail_container, fragment).commit();
+    }
 }
