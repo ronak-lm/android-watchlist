bac6c0e7b1327a7b7b198f467ede8f4abd09caf1
Ronak Manglani <ronak.manglani@gmail.com>
2016-02-05 21:48:27 +0530

    Create videos activity

---
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/VideosActivity.java
@@ -0,0 +1,22 @@
+package com.ronakmanglani.watchlist.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.ronakmanglani.watchlist.R;
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
