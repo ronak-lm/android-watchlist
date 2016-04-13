68f42f7ad8aded176ea99db80898dcc3e68ba993
Ronak Manglani <ronak.manglani@gmail.com>
2016-04-13 20:39:19 +0530

    Add EditText in toolbar for SearchActivity

--- app/src/main/java/com/ronakmanglani/watchlist/activity/SearchActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/SearchActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.ronakmanglani.watchlist.R;
 
@@ -14,14 +16,26 @@ import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
 
-    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.toolbar)     Toolbar toolbar;
+    @Bind(R.id.search_bar)  EditText searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
