1071752a179813baae70bea763600b04ec4bee60
Ronak Manglani <ronak.manglani@gmail.com>
2016-04-14 22:04:28 +0530

    Add ButterKnife binds for search fragment

--- app/src/main/java/com/ronakmanglani/watchlist/fragment/SearchFragment.java
+++ app/src/main/java/com/ronakmanglani/watchlist/fragment/SearchFragment.java
@@ -3,18 +3,34 @@ package com.ronakmanglani.watchlist.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ronakmanglani.watchlist.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SearchFragment extends Fragment {
+
+    @Bind(R.id.error_message)       View errorMessage;
+    @Bind(R.id.progress_circle)     View progressCircle;
+    @Bind(R.id.loading_more)        View loadingMore;
+    @Bind(R.id.search_list)         RecyclerView recyclerView;
 
     // Fragment lifecycle
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, v);
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
     }
 }
