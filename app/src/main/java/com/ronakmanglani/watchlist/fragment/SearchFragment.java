5c8e91fdc2632375b562ce60a532fe5bfa00622b
Ronak Manglani <ronak.manglani@gmail.com>
2016-04-14 21:05:15 +0530

    Create search fragment

---
+++ app/src/main/java/com/ronakmanglani/watchlist/fragment/SearchFragment.java
@@ -0,0 +1,20 @@
+package com.ronakmanglani.watchlist.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ronakmanglani.watchlist.R;
+
+public class SearchFragment extends Fragment {
+
+    // Fragment lifecycle
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        return v;
+    }
+}
