b2919858b104591b8015ccc4e7ca1c9d9acb7c4d
Ronak Manglani <ronak.manglani@gmail.com>
2016-04-14 21:06:40 +0530

    Set key listener for search bar

--- app/src/main/java/com/ronakmanglani/watchlist/activity/SearchActivity.java
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/SearchActivity.java
@@ -1,23 +1,23 @@
 package com.ronakmanglani.watchlist.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import com.ronakmanglani.watchlist.R;
+import com.ronakmanglani.watchlist.fragment.SearchFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
 
-    @Bind(R.id.toolbar)     Toolbar toolbar;
-    @Bind(R.id.search_bar)  EditText searchBar;
+    @Bind(R.id.toolbar)         Toolbar toolbar;
+    @Bind(R.id.search_bar)      EditText searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,18 @@ public class SearchActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    SearchFragment fragment = new SearchFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_search, fragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
