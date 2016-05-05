c24b6d5788bb0eb45286d63e68c607afea57704b
Ronak Manglani <ronak.manglani@gmail.com>
2016-05-05 20:17:05 +0530

    Create about activity

---
+++ app/src/main/java/com/ronakmanglani/watchlist/activity/AboutActivity.java
@@ -0,0 +1,27 @@
+package com.ronakmanglani.watchlist.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.ronakmanglani.watchlist.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+}
